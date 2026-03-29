a4ead99c537ae702f51150e3ff75d7f7804a77f6
MayurGavas <mayurgavas1@gmail.com>
2026-03-29 12:47:55 +0530

    secind  commit6

--- tests/feature1.spec.js
+++ tests/feature1.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test("Feature file 1 : Scenario 1", async({page}) =>{
+test("@smoke Feature file 1 : Scenario 1", async({page}) =>{
 
     await page.goto("https://selectorshub.com/")
     await expect(page).toHaveTitle("SelectorsHub- Free Productivity Booster Tools For Testers")
